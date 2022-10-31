1a5e911d735695dbdbfe7a96952c0ff78dbd4f84
Nicolas Echeverry <nikoxx9900@gmail.com>
2022-10-31 11:58:43 -0500

    fix env variable

--- components/chatdesk/MessageMedia.vue
+++ components/chatdesk/MessageMedia.vue
@@ -59,7 +59,7 @@ export default {
         })
         const form = new FormData()
         form.append('files', await media.blob(), `${this.payload.entry[0].changes[0].value.messages[0].image.id}.${media.type === 'image/jpeg' ? 'jpg' : 'png'}`)
-        await fetch('http://localhost:1337/api/upload', {
+        await fetch(`${this.$config.API_STRAPI_ENDPOINT}upload`, {
           method: 'post',
           headers: {
             Authorization: `Bearer ${this.$store.state.auth.token}`
@@ -69,7 +69,7 @@ export default {
           .then(res => res.json())
           .then((res) => {
             this.createMediaEntry(res[0], this.payload.entry[0].changes[0].value.messages[0].image.id)
-            this.imgBlob = 'http://localhost:1337' + res[0].url
+            this.imgBlob = `${this.$config.CDN_STRAPI_ENDPOINT}${res[0].url}`
           })
       }
     },
