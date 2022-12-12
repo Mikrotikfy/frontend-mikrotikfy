de646b908d4239b14691b67c6b00f4199fe5b0e1
Nicolas Echeverry <nikoxx9900@gmail.com>
2022-12-11 21:40:53 -0500

    fix(frontend): :ambulance: v-row causing app overflow by not having a container

--- components/main/SearchBox.vue
+++ components/main/SearchBox.vue
@@ -1,47 +1,49 @@
 <template>
-  <v-row class="mb-1 justify-center w-100">
-    <v-col
-      cols="12"
-      xs="12"
-      sm="8"
-      md="6"
-      lg="6"
-      xl="6"
-    >
-      <v-row
-        class="mx-1 mt-4 justify-center d-flex"
+  <v-container fluid>
+    <v-row class="mb-1 justify-center w-100">
+      <v-col
+        cols="12"
+        xs="12"
+        sm="8"
+        md="6"
+        lg="6"
+        xl="6"
       >
-        <v-btn
-          color="grey lighten-4 black--text elevation-0"
-          dark
-          :loading="loadingDataTable"
-          tile
-          large
-          x-large
-          style="border-radius: 30px 0 0 30px;padding:5px;height:56px;"
-          @click="getClientBySearch()"
+        <v-row
+          class="mx-1 mt-4 justify-center d-flex"
         >
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-        <v-text-field
-          ref="searchClient"
-          v-model="searchClientInput"
-          :label="loadingDataTable ? 'Cargando... Por favor espere.' : 'Buscar por código, nombre, barrio o dirección'"
-          single-line
-          hide-details
-          filled
-          rounded
-          autofocus
-          autocomplete="off"
-          :loading="loadingDataTable"
-          :disabled="loadingDataTable"
-          class="white--text"
-          style="width:100px;max-width: 600px;border-radius: 0 30px 30px 0;"
-          @keyup.enter="getClientBySearch()"
-        />
-      </v-row>
-    </v-col>
-  </v-row>
+          <v-btn
+            color="grey lighten-4 black--text elevation-0"
+            dark
+            :loading="loadingDataTable"
+            tile
+            large
+            x-large
+            style="border-radius: 30px 0 0 30px;padding:5px;height:56px;"
+            @click="getClientBySearch()"
+          >
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+          <v-text-field
+            ref="searchClient"
+            v-model="searchClientInput"
+            :label="loadingDataTable ? 'Cargando... Por favor espere.' : 'Buscar por código, nombre, barrio o dirección'"
+            single-line
+            hide-details
+            filled
+            rounded
+            autofocus
+            autocomplete="off"
+            :loading="loadingDataTable"
+            :disabled="loadingDataTable"
+            class="white--text"
+            style="width:100px;max-width: 600px;border-radius: 0 30px 30px 0;"
+            @keyup.enter="getClientBySearch()"
+          />
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
