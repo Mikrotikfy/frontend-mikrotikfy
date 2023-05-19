3e33b29acd79cbd8a4d7e36542b2e33217ce879b
Nicolas Echeverry <nikoxx9900@gmail.com>
2023-05-19 18:12:24 -0500

    feat(frontend): :art: Ticket create now shows ticket history

---
+++ components/misc/TicketHistoryTicket.vue
@@ -0,0 +1,243 @@
+<template>
+  <span>
+    <v-card
+      :loading="loading"
+    >
+      <v-card-title>
+        Historial de Tickets
+        <v-spacer />
+        <v-btn icon @click="modal = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-card-title>
+      <div v-if="!loading">
+        <v-card-text>
+          <client-only>
+            <v-data-table
+              :headers="$route.query.clienttype === 'INTERNET' ? headers : headersTV"
+              :items="tickets"
+              :items-per-page="itemsPerPage"
+              :item-class="rowStyles"
+              sort-by="createdAt"
+              calculate-widths
+              sort-desc
+              :page.sync="page"
+              no-data-text="No hay tickets para mostrar aún..."
+              loading-text="Cargando información de tickets..."
+              dense
+              hide-default-footer
+              mobile-breakpoint="100"
+              @page-count="pageCount = $event"
+            >
+              <template v-slot:[`item.actions`]="props">
+                <TicketAdvanceHistory
+                  :ticketid="props.item.id"
+                  :name="props.item.client.name"
+                />
+              </template>
+              <template v-slot:[`item.active`]="props">
+                <v-chip small :color="getColor(props.item.active)" class="white--text">
+                  {{ getState(props.item.active) }}
+                </v-chip>
+              </template>
+              <template v-slot:[`item.createdAt`]="{ item }">
+                <span>
+                  {{ getDate(item.createdAt) }}
+                </span>
+              </template>
+              <template v-slot:[`item.channel`]="{ item }">
+                <v-tooltip v-if="item.tickettype ? item.tickettype.requireverification : false" bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-chip
+                      v-bind="attrs"
+                      small
+                      :color="getChannelColor(item.tickettype, item.reboot, item.network, item.on)"
+                      class="white--text"
+                      v-on="on"
+                    >
+                      <h5>
+                        {{ item.channel ? getChannelName(item.channel) : 'No reg.' }}
+                      </h5>
+                    </v-chip>
+                  </template>
+                  <span>
+                    <ul>
+                      <li :class="item.reboot ? 'green darken-4 px-2 rounded-xl mb-2' : 'red darken-4 px-2 rounded-xl mb-2'">Se reinicio el Router/ONU? {{ item.reboot ? 'SI': 'NO' }}</li>
+                      <li :class="item.network ? 'green darken-4 px-2 rounded-xl mb-2' : 'red darken-4 px-2 rounded-xl mb-2'">Se verifico conexion a la red WiFi? {{ item.network ? 'SI': 'NO' }}</li>
+                      <li :class="item.on ? 'green darken-4 px-2 rounded-xl mb-2' : 'red darken-4 px-2 rounded-xl mb-2'">El equipo esta encendido? {{ item.on ? 'SI': 'NO' }}</li>
+                    </ul>
+                  </span>
+                </v-tooltip>
+                <v-chip
+                  v-else
+                  small
+                  :color="getChannelColor(item.tickettype, item.reboot, item.network, item.on)"
+                  class="white--text"
+                >
+                  <h5>
+                    {{ item.channel ? getChannelName(item.channel) : 'N/A' }}
+                  </h5>
+                </v-chip>
+              </template>
+            </v-data-table>
+          </client-only>
+          <div class="text-center pt-2">
+            <v-pagination v-model="page" :length="pageCount" />
+          </div>
+        </v-card-text>
+      </div>
+    </v-card>
+  </span>
+</template>
+
+<script>
+import TicketAdvanceHistory from '../misc/TicketAdvanceHistory'
+export default {
+  name: 'TicketHistory',
+  components: {
+    TicketAdvanceHistory
+  },
+  props: {
+    clientid: {
+      type: Number,
+      default: -1
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    block: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: () => ({
+    modal: false,
+    loading: true,
+    page: 1,
+    pageCount: 0,
+    itemsPerPage: 10,
+    tickets: [],
+    headers: [
+      { text: 'Estado', sortable: true, value: 'active' },
+      { text: 'Tipo', sortable: true, value: 'tickettype.name' },
+      { text: 'Operador', sortable: false, value: 'assignated.username' },
+      { text: 'Detalles', sortable: true, value: 'details' },
+      { text: 'Potencia Optica', sortable: true, value: 'opticalpower' },
+      { text: 'Canal', sortable: false, value: 'channel', width: 60, align: ' d-none d-lg-table-cell' },
+      { text: 'Creado', sortable: true, value: 'createdAt' },
+      { text: 'Acciones', sortable: true, value: 'actions' }
+    ],
+    headersTV: [
+      { text: 'Estado', sortable: true, value: 'active' },
+      { text: 'Cliente', sortable: true, value: 'client.name' },
+      { text: 'Tipo', sortable: true, value: 'tickettype.name' },
+      { text: 'Operador', sortable: false, value: 'assignated.username' },
+      { text: 'Detalles', sortable: true, value: 'details' },
+      { text: 'Televisores', sortable: true, value: 'tvspec.tvs' },
+      { text: 'Altos', sortable: true, value: 'tvspec.high' },
+      { text: 'Bajos', sortable: true, value: 'tvspec.down' },
+      { text: 'Calidad', sortable: true, value: 'tvspec.tvspectype.name' },
+      { text: 'Canal', sortable: false, value: 'channel', width: 60, align: ' d-none d-lg-table-cell' },
+      { text: 'Creado', sortable: true, value: 'createdAt' },
+      { text: 'Acciones', sortable: true, value: 'actions' }
+    ]
+  }),
+  mounted () {
+    this.initComponent()
+  },
+  methods: {
+    async initComponent () {
+      this.modal = true
+      this.loading = false
+      const qs = require('qs')
+      const query = qs.stringify({
+        filters: {
+          client: {
+            id: {
+              $eq: this.clientid
+            }
+          },
+          clienttype: {
+            name: this.$route.query.clienttype
+          }
+        },
+        populate: [
+          'client',
+          'tickettype',
+          'tvspec',
+          'tvspec.tvspectype',
+          'assignated'
+        ],
+        sort: 'createdAt:desc'
+      },
+      {
+        encodeValuesOnly: true
+      })
+      await fetch(`${this.$config.API_STRAPI_ENDPOINT}tickets?${query}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.$store.state.auth.token}`
+        }
+      })
+        .then(res => res.json())
+        .then((tickets) => {
+          this.tickets = tickets.data
+        })
+    },
+    rowStyles (item) {
+      return ['mb-4', 'rounded-xl', 'grey darken-4']
+    },
+    getDate (date) {
+      const dateObject = new Date(date)
+      const humanDateFormat = dateObject.toLocaleString('es-ES', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric' })
+      return humanDateFormat
+    },
+    getColor (state) {
+      if (state) {
+        return 'blue darken-4'
+      } else {
+        return 'red'
+      }
+    },
+    getState (state) {
+      if (state) {
+        return 'Abierto'
+      } else {
+        return 'Cerrado'
+      }
+    },
+    getChannelColor (tickettype, reboot, network, on) {
+      if (tickettype) {
+        if (!tickettype.requireverification) {
+          return 'grey darken-4'
+        }
+        if (reboot && network && on) {
+          return 'green darken-4'
+        } else {
+          return 'red darken-4'
+        }
+      } else {
+        return 'grey darken-4'
+      }
+    },
+    getChannelName (channel) {
+      if (channel === 'phone') {
+        return 'TELEF.'
+      } else if (channel === 'office') {
+        return 'OFICINA'
+      } else if (channel === 'whatsapp') {
+        return 'WHASTAPP'
+      } else if (channel === 'email') {
+        return 'EMAIL'
+      } else {
+        return 'OTRO'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ components/misc/UserRecomendation.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <v-col v-if="$route.query.clienttype === 'INTERNET'" cols="12" md="6" lg="7">
+      <v-card class="rounded-xl">
+        <v-card-title>
+          <v-icon class="mr-2">
+            mdi-account-alert-outline
+          </v-icon>
+          Recomendaciones para la toma de casos nuevos
+        </v-card-title>
+        <v-card-text>
+          <span class="text-body-1 text-decoration-underline red--text">Preguntas a realizar al ENCONTRARSE FUERA DE LINEA ❌</span><br>
+          <span class="text-body-1 ml-2">• ¿Ya realizo un reinicio del router?</span><br>
+          <span class="text-body-1 ml-10">• Si no lo ha realizado, pedirle que lo realice. Muchas veces el router se encuentra bloqueado.</span><br>
+          <span class="text-body-1 ml-2">• ¿Tiene una luz roja encendida en el router?</span><br>
+          <span class="text-body-1 ml-10">• Luz roja indica perdida de potencia óptica. Tomar revisión.</span><br>
+          <span class="text-body-1 ml-2">• ¿Le aparece la red WIFI?</span><br>
+          <span class="text-body-1 ml-10">• Si no le aparece la red WIFI significa que el router pudo haberse reseteado de fábrica. Tomar Revisión</span><br>
+          <span class="text-body-1 ml-2">• ¿Todos los cables se encuentran correctamente conectados?</span><br>
+          <span class="text-body-1 ml-10">• El usuario pudo haber manipulado el orden de conexión de los cables de router generando la perdida de señal. Si lo hizo, tomar revisión.</span><br>
+          <span class="text-body-1 ml-2">• ¿Esta sobrecalentado el router?</span><br>
+          <span class="text-body-1 ml-10">• Si esta sobrecalentado puede presentar problemas de cobertura o navegación. Tomar revisión.</span><br><br>
+          <span class="text-body-1 text-decoration-underline green--text">Preguntas a realizar al ENCONTRARSE EN LINEA ✔️: </span><br>
+          <span class="text-body-1 ml-2">• ¿En el momento cuenta con navegación?</span><br>
+          <span class="text-body-1 ml-10">• Verificar si necesita algún servicio relacionado con cambio de contraseña o nombre de red</span><br>
+          <span class="text-body-1 ml-2">• ¿Desde que fecha presenta el inconveniente?</span><br>
+          <span class="text-body-1 ml-10">• Asegurarse que la revisión se pide en fechas que no concuerden con caídas de azteca recientes. De ser el caso, explicarle al cliente que se han presentado fallas por parte del proveedor. Si son antes de las fechas de caídas, tomar la revisión.</span><br>
+          <span class="text-body-1 ml-2">• ¿Es en todos los dispositivos, o solo en algunos específicos?</span><br>
+          <span class="text-body-1 ml-10">• Si los errores de navegación son solo en unos dispositivos en específico, puede ser problemas de canal y banda WIFI. Tomar la revisión</span><br>
+          <span class="text-body-1 ml-10">• Si es en todos los dispositivos, puede ser que se haya solicitado un cambio de contraseña recientemente y el usuario no haya realizado el cambio en los dispositivos. Si no se pidió un cambio de contraseña recientemente, Tomar revisión.</span><br>
+          <span class="text-body-1 ml-2">• ¿A que red esta conectado en el dispositivo que no le navega?</span><br>
+          <span class="text-body-1 ml-10">• El usuario puede no estar conectado a la red WIFI. Preguntar a que red esta conectada y si concuerda con su apellido para descartar este escenario. O puede estar conectado a la red de algún vecino del que tenga la contraseña.</span><br>
+          <span class="text-body-1 ml-2">• ¿Ha solicitado un cambio de contraseña recientemente?</span><br>
+          <span class="text-body-1 ml-10">• A veces el usuario olvida que pidió un cambio de contraseña y no la agrega a sus dispositivos de nuevo, quedándose sin navegación.</span>
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col v-if="$route.query.clienttype === 'TELEVISION'" cols="12" md="6" lg="7">
+      <v-card class="rounded-xl">
+        <v-card-title>
+          <v-icon class="mr-2">
+            mdi-account-alert-outline
+          </v-icon>
+          Recomendaciones para la toma de casos nuevos
+        </v-card-title>
+        <v-card-text>
+          <span class="text-body-1 text-decoration-underline green--text">Preguntas a realizar al Cliente ✔️: </span><br>
+          <span class="text-body-1 ml-2">• ¿Afecta solo 1 canal o es en todos?</span><br>
+          <span class="text-body-1 ml-10">• Verificar esto para determinar si es el nodo o amplificador</span><br>
+          <span class="text-body-1 ml-2">• ¿Desde que fecha presenta el inconveniente?</span><br>
+          <span class="text-body-1 ml-10">• Asegurarse que la revisión se pide en fechas que no concuerden con trabajos de adecuacion de red o de terceros.</span><br>
+          <span class="text-body-1 ml-2">• ¿Aparece un letrero de "Codificado" o similar?</span><br>
+          <span class="text-body-1 ml-10">• Puede tratarse de una revision por solo 1 canal que tenga afectado la señal satelital.</span><br>
+          <span class="text-body-1 ml-2">• ¿Esta conectado el Televisor al cable de la parabolica?</span><br>
+          <span class="text-body-1 ml-10">• El usuario pudo haber desconectado el cable por alguna limpieza o cambio de lugar que haya realizado y olvido volverlo a conectar</span><br>
+          <span class="text-body-1 ml-2">• ¿Sucede en todos los televisores o solo en 1?</span><br>
+          <span class="text-body-1 ml-10">• Si presenta el problema solo en 1 televisor puede significar daño en conector o cable.</span>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </div>
+</template>
