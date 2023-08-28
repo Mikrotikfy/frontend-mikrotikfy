d0b876f24ea9202a3ed620afdea50c9d3be34497
Nicolas Echeverry <nikoxx9900@gmail.com>
2023-08-28 11:24:15 -0500

    update counter

--- static/counter.js
+++ static/counter.js
@@ -1,22 +1,7 @@
 const http = require('http')
 const refresh1 = () => {
-  http.get('http://localhost:1337/refreshClientData?city=5f832e8fb0c43e2c64b37437', function (response) {
-    return response
-  })
-  http.get('http://localhost:1337/refreshClientData?city=5fc3f0408e3de73d204cd430', function (response) {
-    return response
-  })
-  http.get('http://localhost:1337/refreshMikrotikData?city=5f832e8fb0c43e2c64b37437', function (response) {
-    return response
-  })
-  http.get('http://localhost:1337/refreshMikrotikData?city=5fc3f0408e3de73d204cd430', function (response) {
+  http.get('http://localhost:1337/api/activeclients?city=1', function (response) {
     return response
   })
 }
-
-const setIntervalImmediately = (func, interval) => {
-  func()
-  return setInterval(func, interval)
-}
-
-setIntervalImmediately(refresh1, 60000)
+refresh1()
