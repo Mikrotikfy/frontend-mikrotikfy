f0e35db5ce17c7a81adecea15ce560c6fbaa5506
Nicolas Echeverry <nikoxx9900@gmail.com>
2023-10-02 11:16:17 -0500

    fix billing state

--- components/billing/Process.vue
+++ components/billing/Process.vue
@@ -85,7 +85,7 @@ export default {
       return this.$store.state.billing.sendIndex
     },
     clients () {
-      return this.$store.state.billing.clients
+      return this.$store.state.billing.clients || []
     },
     month () {
       return this.$store.state.billing.month
